36641814a214eb056142f93499ddf39c8ce7f00b
Flávio Félix <fau_felix30@hotmail.com>
2025-01-29 15:38:45 -0300

    Começando a fazer a lógica do secret words

--- src/App.js
+++ src/App.js
@@ -22,8 +22,41 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(category, word);
+
+    return { category, word };
+  };
+
   // Start secrets word game
   const startGame = () => {
+    // Pick word and category
+    const { word, category } = pickWordCategory();
+
+    // Create an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // Fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
