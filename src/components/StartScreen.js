3aa4bfa813dcf41096ce03620a68baa2bf9e506b
Flávio Félix <fau_felix30@hotmail.com>
2025-01-29 14:34:45 -0300

    Feito o primeiro componente startScreen da página inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo pra começar a jogar</p>
+      <button>Começa o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
