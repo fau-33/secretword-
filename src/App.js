751025739da239a5cad228455c7b25e98fcc6f82
Flávio Félix <fau_felix30@hotmail.com>
2025-01-29 15:08:56 -0300

    Feito a troca de páginas com os componentes

--- src/App.js
+++ src/App.js
@@ -21,13 +21,27 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  console.log(words);
+
+  // Start secrets word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  // Restart the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import "./StartScreen.css";
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
   return (
     <div className="start">
       <h1>Secret Word</h1>
       <p>Clique no botão abaixo pra começar a jogar</p>
-      <button>Começa o jogo</button>
+      <button onClick={startGame}>Começa o jogo</button>
     </div>
   );
 };
